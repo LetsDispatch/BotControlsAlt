119666ae5edf1693f6e642f2b840d67a2fe6cda3
Dispatch Dev <letsdispatchme@gmail.com>
2026-01-23 23:12:13 +0200

    Update onload.js

--- onload.js
+++ onload.js
@@ -650,7 +650,7 @@ function renderBotUsers(botUsers){
 
 //requests
 
-const baseAddress = "https://4cc012f1c22c.ngrok-free.app/web"
+const baseAddress = "https://ee9b3519b8ed.ngrok-free.app/web"
 axios.defaults.headers.common['ngrok-skip-browser-warning'] = '69420'
 let token = '';
 
